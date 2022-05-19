7ea62948152c7c956271c2ac505a201bbcca5215
juagudeloo <juagudeloo@unal.edu.co>
2022-05-18 22:58:49 -0500

    fix

--- Python3-MODELS/Juan_Esteban/3-Big_test_v1/data_functions.py
+++ Python3-MODELS/Juan_Esteban/3-Big_test_v1/data_functions.py
@@ -110,6 +110,8 @@ def charge_data(self_ptm:str, self_filename:str, self_nx:int, self_ny:int, self_
 
         #         self.mvxx=self.mvxx/self.mrho
         if np.any(self_mrho[i] == 0):
+            n0 = np.argwhere(self_iout == 0)
+            print(n0)
             self_iout.pop()
             self_mrho.pop()
             self_mtpr.pop()
