db5b2f55e4bec6e6d5dc2b92a3ee7ba93a66bd3e
juagudeloo <juagudeloo@unal.edu.co>
2022-05-18 22:40:41 -0500

    fix

--- Python3-MODELS/Juan_Esteban/3-Big_test_v1/data_functions.py
+++ Python3-MODELS/Juan_Esteban/3-Big_test_v1/data_functions.py
@@ -109,6 +109,7 @@ def charge_data(self_ptm:str, self_filename:str, self_nx:int, self_ny:int, self_
 
         #         self.mvxx=self.mvxx/self.mrho
         if np.any(self_mrho[i] == 0):
+            print(np.shape(self_mrho))
             self_mrho.pop()
             self_iout.pop()
             self_mrho.pop()
