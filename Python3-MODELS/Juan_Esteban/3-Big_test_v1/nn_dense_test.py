df8bc5dadcf203f8cc92bdaee3712045958350bd
juagudeloo <juagudeloo@unal.edu.co>
2022-05-23 10:38:45 -0500

    allocated memory growths

--- Python3-MODELS/Juan_Esteban/3-Big_test_v1/nn_dense_test.py
+++ Python3-MODELS/Juan_Esteban/3-Big_test_v1/nn_dense_test.py
@@ -16,8 +16,18 @@ import testing_functions as test
 
 #################################################################### MAIN FUNCTION ####################################################################
 def main():
-    config = tf.compat.v1.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = 0.4
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
 
     path = '/mnt/scratch/juagudeloo/Total_MURAM_data/' #Juan Esteban path
     self_ptm = path
