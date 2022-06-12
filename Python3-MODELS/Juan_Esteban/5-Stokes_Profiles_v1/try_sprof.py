e2d94e4a4d173591f4dc70802293584d88c1829b
juagudeloo <juagudeloo@unal.edu.co>
2022-06-11 20:01:33 -0500

    fix plotting

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
@@ -14,10 +14,11 @@ def main():
         prof_im.append(mprof.read_prof(path+file, 'nicole',  nx, ny, nlam, ix, iy))
     fig, ax = plt.subplots(2, 2, figsize = (10,10))
     
-    for i in range(int(len(prof_im)/2)):
-        print(np.shape(prof_im[i]))
-        x = range(0, len(prof_im[i]))
-        ax[i%2, i].scatter(x,prof_im[i])
+    for i in range(int(len(prof_im))):
+        for j in range(int(len(prof_im)/2)):
+            print(np.shape(prof_im[i+j]))
+            x = range(0, len(prof_im[i+j]))
+            ax[i, j].scatter(x,prof_im[i+j])
     fig.savefig("first_prof.png")
     
     
