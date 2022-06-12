d8ba3dfa33e21fec8fac907a563726da3c92d764
juagudeloo <juagudeloo@unal.edu.co>
2022-06-11 19:59:30 -0500

    fix append

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
@@ -9,13 +9,13 @@ def main():
     ny = 480
     nlam = 300
     prof_im = []
-    for i in range(0, 3):
+    for i in range(0, 4):
         ix, iy = [i,i]
         prof_im.append(mprof.read_prof(path+file, 'nicole',  nx, ny, nlam, ix, iy))
     fig, ax = plt.subplots(2, 2, figsize = (10,10))
     
     for i in range(int(len(prof_im)/2)):
-        print(np.shape(prof_im))
+        print(np.shape(prof_im[i]))
         x = range(0, len(prof_im[i]))
         ax[i%2, i].scatter(x,prof_im[i])
     fig.savefig("first_prof.png")
