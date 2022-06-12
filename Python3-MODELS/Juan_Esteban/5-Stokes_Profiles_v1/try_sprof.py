091025a852adb78dc3074068e5e141cf4cfd40dd
juagudeloo <juagudeloo@unal.edu.co>
2022-06-12 15:40:57 -0500

    various lambda values

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof.py
@@ -26,18 +26,20 @@ def main():
     for n in range(N_profs):
         prof_im.append(np.zeros((nx, ny)))
     
-    for n in range(N_profs): 
-        for i in range(nx):
-            for j in range(ny):
-                prof_im[n][i,j] = profs[i+j][n,0]
-    
-    #Plotting the four profiles 
+    #Plotting the four profiles for the 300 lambda values
     title = ['I', 'Q', 'U', 'V']
-    fig, ax = plt.subplots(1, 4, figsize=(10,10))
-    for n in range(N_profs):
-        ax[n].imshow(prof_im[n], cmap = 'Greys')
-        ax[n].set_title(title[n])
-    fig.savefig('Images/params_0-052.png')
+    for lam in range(nlam):
+        for n in range(N_profs): 
+            for i in range(nx):
+                for j in range(ny):
+                    prof_im[n][i,j] = profs[i+j][n,lam]
+        
+        #Plotting the four profiles 
+        fig, ax = plt.subplots(1, 4, figsize=(10,40))
+        for n in range(N_profs):
+            ax[n].imshow(prof_im[n], cmap = 'Greys')
+            ax[n].set_title(title[n])
+        fig.savefig(f'Images/params_0-052-lam_{lam}.png')
     
     
 if __name__ == "__main__":
