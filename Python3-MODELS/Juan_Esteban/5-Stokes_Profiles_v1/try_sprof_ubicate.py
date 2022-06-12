1934ea198f5a4ecbdf80ff9d025d2afbe25cf2ba
juagudeloo <juagudeloo@unal.edu.co>
2022-06-12 18:31:42 -0500

    comparing different ubications

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof_ubicate.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/try_sprof_ubicate.py
@@ -20,18 +20,20 @@ def main():
     
     print(np.shape(profs))
     
-    ix = 200
-    iy = 200
+    ix1, iy1 = [200,200]
+    ix2, iy2 = [400,300]
     nlam_inf = 6300.5
     nlam_sup = nlam_inf+nlam*0.01
     x = np.linspace(nlam_inf,nlam_sup,nlam)
     
     title = ['I', 'Q', 'U', 'V']
-    fig, ax = plt.subplots(1,4, figsize = (40, 10))
+    fig, ax = plt.subplots(2,4, figsize = (40, 20))
     for prof in range(N_profs):
-        ax[prof].scatter(x, profs[ix+iy][prof, :])
-        ax[prof].set_title(title[prof])
-    fig.savefig(f'Images/profiles-ix_{ix}-iy_{iy}.png')
+        ax[0,prof].scatter(x, profs[ix1+iy1][prof, :])
+        ax[0,prof].set_title(title[prof])
+        ax[1,prof].scatter(x, profs[ix2+iy2][prof, :])
+        ax[1,prof].set_title(title[prof])
+    fig.savefig(f'Images/profiles-ix1_{ix1}-iy1_{iy1}-ix2_{ix2}-iy2_{iy2}.png')
     
 if __name__ == "__main__":
     main()
