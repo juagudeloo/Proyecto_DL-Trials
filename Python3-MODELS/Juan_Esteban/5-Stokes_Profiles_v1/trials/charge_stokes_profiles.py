e29fca8f697a53611e8c86e33dad64ee8972131e
juagudeloo <juagudeloo@unal.edu.co>
2022-06-14 19:42:53 -0500

    doing trials of charging the profiles

---
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v1/trials/charge_stokes_profiles.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import model_prof_tools as mprof
+
+def main():
+    #extracting parameters
+    path = "/mnt/scratch/juagudeloo/Stokes_profiles/PROFILES"
+    filename = path+"052000_0000_0000.prof"
+    filetype = "nicole"
+    nlam = 300
+    sequential = 1
+    nx, ny = [480, 480]
+    ix, iy = [0,0]
+
+    profiles = mprof.read_prof(filename, filetype, nx, ny, nlam, ix, iy, sequential)
+    print(np.shape(profiles))
+    
+if __name__ == "__main__":
+    main()
