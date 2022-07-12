674a8059fc7748e4ad3f7e360ab8bf7dc1aafe79
juagudeloo <juagudeloo@unal.edu.co>
2022-07-12 18:04:29 -0500

    testing

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4_alterno/data_class.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4_alterno/data_class.py
@@ -177,7 +177,7 @@ class Data_NN_model():
         self.model.compile(optimizer = opt, loss = loss, metrics = loss)
         print("model compiled!")
         training = tf.data.Dataset.from_tensor_slices((self.tr_input, self.tr_output))
-        self.history = self.model.fit(training, epochs=10, batch_size=2, verbose=1)
+        self.history = self.model.fit(self.tr_input, self.tr_output, epochs=10, batch_size=2, verbose=1)
         self.model.evaluate(self.te_input, self.tr_output)
     def plot_loss(self):
         fig,ax = plt.subplots(figsize = (10,7))
