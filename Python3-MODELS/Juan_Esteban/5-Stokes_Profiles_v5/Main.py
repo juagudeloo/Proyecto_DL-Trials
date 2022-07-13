e91fd1e948e1ff967ce311ff5fa732604c9d2079
juagudeloo <juagudeloo@unal.edu.co>
2022-07-13 01:18:16 -0500

    trying more epochs

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
+from nn_model import NN_model
 
 def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = []
-    for i in range(53,55):
+    for i in range(53,56):
         a = "0"+str(i)+"000"
         tr_filename.append(a)
     IN_LS = np.array([4,256]) #input shape in input layer
@@ -14,11 +14,11 @@ def main():
     sun_model = NN_model("Stokes params")
     sun_model.compile_model(IN_LS)
     for fln in tr_filename:
-        sun_model.train(fln, tr_s = 0.75, batch_size_percentage = 0.05, epochs=3)
+        sun_model.train(fln, tr_s = 0.75, batch_size_percentage = 0.05, epochs=10)
         sun_model.plot_loss()
     #Model predicting
     pr_filename = []
-    for i in range(56,57):
+    for i in range(56,58):
         a = "0"+str(i)+"000"
         pr_filename.append(a)
     
