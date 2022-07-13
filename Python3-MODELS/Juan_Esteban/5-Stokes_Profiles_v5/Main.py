daf6796c82eb066886fb52255b56235b173cc73a
juagudeloo <juagudeloo@unal.edu.co>
2022-07-13 00:40:07 -0500

    teting Stokes params

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
@@ -13,7 +13,7 @@ def main():
         tr_filename.append(a)
     IN_LS = np.array([4,256]) #input shape in input layer
     #Model training
-    sun_model = NN_model("Intensity")
+    sun_model = NN_model("Stokes params")
     sun_model.compile_model(IN_LS)
     for fln in tr_filename:
         sun_model.train(fln, tr_s = 0.75, batch_size_percentage = 0.05, epochs=3)
