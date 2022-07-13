e2e2147630d9b93f37b4e2cc7e2c1b88ca7c8fee
juagudeloo <juagudeloo@unal.edu.co>
2022-07-12 19:07:46 -0500

    debugging

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4/trial.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4/trial.py
@@ -7,6 +7,7 @@ from sklearn.metrics import r2_score
 from data_class import Data_NN_model
 
 def main():
+    #model to test
     IN_LS = (4,256) #input shape in input layer
     print("compiling the model...")
     model = tf.keras.Sequential()
@@ -23,13 +24,23 @@ def main():
     loss = tf.keras.metrics.MeanSquaredError()
     model.compile(optimizer = opt, loss = loss, metrics = loss)
     print("model compiled!")
-    print(model.summary())
+    model.summary()
 
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = "053000"
     
+    #charging the data
     data_obj = Data_NN_model(IN_LS, 1, "Intensity")
+    tr_inputs = data_obj.charge_inputs(tr_filename)
+    tr_outputs = data_obj.charge_intensity(tr_filename)
+    print(np.shape(tr_inputs))
+    print(type(tr_inputs))
+    print(np.shape(tr_outputs))
+    print(type(tr_outputs))
+
+
+    
 
 if __name__ == "__main__":
     main()
