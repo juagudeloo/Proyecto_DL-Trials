68a168fadbba5b16a54a818bf1fed358c7b2031d
juagudeloo <juagudeloo@unal.edu.co>
2022-07-12 22:05:53 -0500

    fix

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4_alter/Main.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4_alter/Main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
 from data_class import Data_class
 import testing_functions as tef
 
@@ -14,20 +15,10 @@ def main():
     IN_LS = np.array([4,256]) #input shape in input layer
     TR_BATCH_SIZE = int(len(TR_D[:,1,2])/1)
 
-    #MODELS DATA DICTIONARY
-    models = []
-    history = []
-    metrics = []
-    intensity_pred = []
-
-    #PLOT INFORMATION
-    titles = ['CNN 1', 'CNN 2', 'CNN 3', 'CNN 4']
-                            
-    dist_name = "nn_conv_models_dist.png"
-    error_fig_name = "nn_conv_models_error.png"
-    ##### NEURAL NETWORK TYPE VARIATIONS
-    #dense models
-    models, metrics, history = tef.test_conv_models(IN_LS, TR_D, TR_L, TR_BATCH_SIZE, TE_D, TE_L)
+    model = tef.model_dense_layers(IN_LS, n_layers = 4)
+    opt_func = tf.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(loss='mean_squared_error', optimizer = opt_func, metrics = [tf.keras.metrics.MeanSquaredError()])
+    model.summary()
 
     
 if __name__ == "__main__":
