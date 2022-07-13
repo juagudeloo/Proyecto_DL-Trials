7065c94654a89da36bf8018b8e754affd94fa8ec
juagudeloo <juagudeloo@unal.edu.co>
2022-07-12 19:02:38 -0500

    debugging

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4/trial.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v4/trial.py
@@ -7,16 +7,17 @@ from sklearn.metrics import r2_score
 from data_class import Data_NN_model
 
 def main():
+    IN_LS = (4,256) #input shape in input layer
     print("compiling the model...")
     model = tf.keras.Sequential()
-    model.add(Conv1D(512, 2, activation='relu', input_shape=self.in_ls))
+    model.add(Conv1D(512, 2, activation='relu', input_shape=IN_LS))
     model.add(Conv1D(256, 2, activation='relu'))
     model.add(Conv1D(128, 1, activation='relu'))
     model.add(Conv1D(64, 2, activation='relu'))
     model.add(GlobalMaxPool1D())
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.3)) #Layer added to avoid the overfitting
-    model.add(Dense(self.out_ls, name="output"))
+    model.add(Dense(1, name="output"))
     lr = 0.001
     opt = tf.keras.optimizers.Adam(learning_rate=lr)
     loss = tf.keras.metrics.MeanSquaredError()
@@ -27,7 +28,7 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = "053000"
-    IN_LS = (4,256) #input shape in input layer
+    
     data_obj = Data_NN_model(IN_LS, 1, "Intensity")
 
 if __name__ == "__main__":
