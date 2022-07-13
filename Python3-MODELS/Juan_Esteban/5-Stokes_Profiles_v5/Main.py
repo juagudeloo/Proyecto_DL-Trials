df33c5fe2bd26671fdf8089a56b3e4b1fbf0e27e
juagudeloo <juagudeloo@unal.edu.co>
2022-07-12 23:18:58 -0500

    rearraging

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v5/Main.py
@@ -8,22 +8,16 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = "053000"
-
-    data = Data_class()
-    TR_D, TR_L, TE_D, TE_L = data.split_data(tr_filename, TR_S = 0.75, output_type="Intensity")
-
     IN_LS = np.array([4,256]) #input shape in input layer
-    TR_BATCH_SIZE = int(len(TR_D[:,1,2])/1)
 
-    model = NN_model(IN_LS, TR_D, TR_L, TE_D, TE_L, TR_BATCH_SIZE)
-    model.model_train()
+    sun_model = NN_model()
+    sun_model.train(tr_filename, "Intensity", tr_s = 0.75, in_ls = IN_LS, batch_size = 0.05)
 
 class NN_model(Data_class):
     def __init__(self, nx = 480, ny = 256, nz = 480):
         super().__init__(nx,ny,nz)
-    def compile_model(self, IN_LS):
-        self.in_ls
-        data_in =  tf.keras.layers.Input(shape = self.in_ls, name='data_in')
+    def compile_model(self, in_ls):
+        data_in =  tf.keras.layers.Input(shape = in_ls, name='data_in')
         dense1 = tf.keras.layers.Conv1D(512, 2, activation=tf.nn.relu)
         dense2 = tf.keras.layers.Conv1D(256, 2, activation=tf.nn.relu)
         dense3 = tf.keras.layers.Conv1D(128, 2, activation=tf.nn.relu)
@@ -42,12 +36,16 @@ class NN_model(Data_class):
 
         self.model = tf.keras.models.Model(inputs = data_in, outputs = x)
         return self.model
-    def model_train(self,filename, output_type, TR_S, IN_LS, TR_BATCH_SIZE):
-        self.split_data(filename, output_type, TR_S)
-        self.compile_model()
+    def train(self,filename, output_type, tr_s, in_ls, batch_size):
+        """
+        batch_size: its a fraction relative to the total of the set (must be between 0<x<1).
+        """
+        self.split_data(filename, output_type, tr_s)
+        self.compile_model(in_ls)
         opt_func = tf.keras.optimizers.Adam(learning_rate=0.001)
         self.model.compile(loss='mean_squared_error', optimizer = opt_func, metrics = [tf.keras.metrics.MeanSquaredError()])
         self.model.summary()
+        TR_BATCH_SIZE = int(self.tr_input[:,1,2].size*batch_size)
         self.model.fit(self.tr_input, self.tr_output, epochs=8, batch_size=TR_BATCH_SIZE, verbose=1)
 
 if __name__ == "__main__":
