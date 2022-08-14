74b9caa7416f94bfd8945891a538b5fdcc666dc0
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-14 03:41:19 -0500

    testing with different hyperparameters

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v1/Main.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v1/Main.py
@@ -6,7 +6,7 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = []
-    for i in range(53,56):
+    for i in range(53,58):
         a = "0"+str(i)+"000"
         tr_filename.append(a)
     IN_LS = np.array([4,300]) #input shape in input layer - Stokes profiles as input
@@ -14,11 +14,11 @@ def main():
     sun_model = NN_model_atm("Stokes params")
     sun_model.compile_model(IN_LS)
     for fln in tr_filename:
-        sun_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=1)
+        sun_model.train(fln, tr_s = 0.75, batch_size= 2, epochs=1000)
         sun_model.plot_loss()
     #Model predicting
     pr_filename = []
-    for i in range(56,58):
+    for i in range(59,61):
         a = "0"+str(i)+"000"
         pr_filename.append(a)
     
