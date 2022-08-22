a59c4fc90ce5804eb779d9665d486974f03ef509
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-21 22:15:47 -0500

    normalization folder

---
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v2/Main.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from nn_model import NN_model_atm
+
+def main():
+    #Intensity specifications
+    ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
+    tr_filename = []
+    for i in range(53,58):
+        a = "0"+str(i)+"000"
+        tr_filename.append(a)
+    IN_LS = np.array([4,300]) #input shape in input layer - Stokes profiles as input
+    #Model training
+    sun_model = NN_model_atm("Stokes params")
+    sun_model.compile_model(IN_LS, learning_rate=0.001)
+    for fln in tr_filename:
+        sun_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=1000)
+        sun_model.plot_loss()
+    #Model predicting
+    pr_filename = []
+    for i in range(59,61):
+        a = "0"+str(i)+"000"
+        pr_filename.append(a)
+    
+    for fln in pr_filename:
+        sun_model.predict_values(fln)
+        sun_model.plot_predict()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v2/nn_model.py
@@ -0,0 +1,90 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from data_class import Data_class
+
+class NN_model_atm(Data_class):
+    def __init__(self, input_type, nx = 480, ny = 256, nz = 480, lower_boundary=180):
+        super().__init__(nx,ny,nz,lower_boundary)
+        self.input_type = input_type
+    def compile_model(self, in_ls, learning_rate=0.001):
+        data_in =  tf.keras.layers.Input(shape = in_ls, name='data_in')
+        conv1 = tf.keras.layers.Conv1D(512, 2, activation=tf.nn.relu)
+        conv2 = tf.keras.layers.Conv1D(256, 2, activation=tf.nn.relu)
+        conv3 = tf.keras.layers.Conv1D(128, 2, activation=tf.nn.relu)
+        conv4 = tf.keras.layers.Conv1D(64, 1, activation=tf.nn.relu) 
+        dropout = tf.keras.layers.Dropout(0.5)
+        flattened = tf.keras.layers.Flatten()  
+        output = tf.keras.layers.Dense(4*(256-self.lb), activation=tf.nn.sigmoid)
+
+        input = conv1(data_in)
+        x = conv2(input)
+        x = conv3(x)
+        x = conv4(x)
+        #x = dropout(x)
+        x = flattened(x) #If this layer is not put, then the output will be of 4 channels......but for some reason is not working here
+        x = output(x)
+
+        self.model = tf.keras.models.Model(inputs = data_in, outputs = x)
+        opt_func = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        self.model.compile(loss='mean_squared_error', optimizer = opt_func, metrics = [tf.keras.metrics.MeanSquaredError()])
+        self.model.summary()
+        
+        return self.model
+    def train(self,filename, tr_s, batch_size, epochs=8):
+        """
+        tr_s: training size percentage
+        """
+        self.split_data(filename, self.input_type, tr_s)
+        self.history = self.model.fit(self.tr_input, self.tr_output[:,], epochs=epochs, batch_size=batch_size, verbose=1)
+        self.model.evaluate(self.te_input, self.te_output)
+    def plot_loss(self):
+        fig,ax = plt.subplots(figsize = (10,7))
+        ax.plot(range(len(self.history.history['loss'])), self.history.history['loss'])
+        ax.set_title("Atmosphere parameters")
+        ax.set_ylabel("Loss")
+        ax.set_xlabel("epochs")
+        if self.input_type == "Intensity":
+            fig.savefig(f"Images/Intensity/loss_plot-{self.filename}.png")
+        if self.input_type == "Stokes params":
+            fig.savefig(f"Images/Stokes_params/loss_plot-{self.filename}.png")
+        print(f"{self.filename} loss plotted!")
+    ##### PREDICTING PHASE #####
+    def predict_values(self, filename):
+        self.pred_filename = filename
+        print(f"{self.pred_filename} predicting...")
+        if self.input_type == "Intensity":
+            self.charge_intensity(self.pred_filename)
+            self.predicted_values = np.memmap.reshape(self.model.predict(self.iout), (self.nx, self.nz, 4, (256-self.lb)))
+            print(f"{self.pred_filename} prediction done!")
+        if self.input_type == "Stokes params":
+            self.charge_stokes_params(self.pred_filename)
+            self.predicted_values = np.memmap.reshape(self.model.predict(self.profs), (self.nx, self.nz, 4, (256-self.lb)))
+            print(f"{self.pred_filename} prediction done!\n")
+        return self.predicted_values
+    def plot_predict(self):
+        N_profs = 4
+        ix = 200
+        iz = 280
+        height = 10
+        title = ['Magnetic Field','Velocity','Density','Temperature']
+        fig, ax = plt.subplots(4,4,figsize=(50,7))
+        original_atm = self.charge_atm_params(self.pred_filename)
+        original_atm = np.memmap.reshape(original_atm, (self.nx, self.nz, 4, (256-self.lb)))
+        for i in range(N_profs):
+            ax[0,i].plot(range(256-self.lb), self.predicted_values[ix,iz,i,:], label="Predicted curve")
+            ax[0,i].set_title(f"Atmosphere parameters height serie - title={title[i]} - ix={ix}, iy={iz}")
+            ax[0,i].plot(range(256-self.lb), original_atm[ix,iz,i,:], label="Original curve")
+            ax[0,i].legend()
+            ax[1,i].imshow(self.predicted_values[:,:,i,height], cmap = "gist_gray")     
+            ax[1,i].set_title(f"Atmosphere parameters spatial distribution- title={title[i]}")
+            ax[2,i].imshow(original_atm[:,:,i,height], cmap = "gist_gray")     
+            ax[2,i].set_title(f"ORIGINAL spatial distribution - title={title[i]}")
+            ax[3,i].imshow(np.abs(np.subtract(original_atm[:,:,i,height],self.predicted_values[:,:,i,height])), cmap = "gist_gray")     
+            ax[3,i].set_title(f"Substraction of both images - title={title[i]}")
+
+        if self.input_type == "Intensity":
+            fig.savefig(f"Images/Intensity/Atmosphere_parameter-{self.filename}.png")
+        if self.input_type == "Stokes params":
+            fig.savefig(f"Images/Stokes_params/Atmosphere_parameter-{self.filename}.png")
+        print(f"{self.pred_filename} prediction plotted\n")
