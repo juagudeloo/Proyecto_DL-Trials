5009f4064485595cff110ad6dc3baa3caf0ffc5a
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-26 16:43:45 -0500

    manual scaling

---
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/scaler_save.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from data_class import Data_class
+
+def main():
+    #Intensity specifications
+    ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
+    tr_filename = []
+    names = ["mbyy", "mvyy", "mrho", "mtpr", "iout", "stokes"]
+    scaler_pairs = {names[0]: [],
+                     names[1]: [],
+                     names[2]: [],
+                     names[3]: [],
+                     names[4]: [],
+                     names[5]: []}
+    dc = Data_class()
+    for i in range(53,254):
+        a = ptm+"0"+str(i)+"000"
+        tr_filename.append(a)
+        dc.charge_atm_params(a)
+        dc.charge_intensity(a)
+        dc.charge_stokes_params(a)
+        for nm in names:
+            scaler_pairs[nm].append(np.load(nm+".npy"))
+    
+    
+
+
+
+if __name__ == "__main__":
+    main()
