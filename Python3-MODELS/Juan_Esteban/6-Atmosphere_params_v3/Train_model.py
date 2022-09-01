f20787b4ca1fc13dd238bfe8dd1394ea275cb553
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-31 21:24:12 -0500

    testing

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
@@ -5,7 +5,7 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = []
-    for i in np.arange(53,99,2):
+    for i in np.arange(53,58,2):
         a = "0"+str(i)+"000"
         tr_filename.append(a)
     IN_LS = np.array([300,4]) #input shape in input layer - Stokes profiles as input
@@ -16,16 +16,6 @@ def main():
         sun_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=1)
         sun_model.plot_loss()
     sun_model.save_model()
-    #Model predicting
-    pr_filename = []
-    for i in range(59,61):
-        a = "0"+str(i)+"000"
-        pr_filename.append(a)
-    
-    for fln in pr_filename:
-        sun_model.predict_values(fln)
-        sun_model.plot_predict()
-
 
 if __name__ == "__main__":
     main()
