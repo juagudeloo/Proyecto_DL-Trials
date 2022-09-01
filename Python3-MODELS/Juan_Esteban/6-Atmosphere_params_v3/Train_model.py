5845337007dea8c581b09a5cc192ae2bb6a81867
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-08-31 23:02:31 -0500

    more files and 10 epochs

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
@@ -5,7 +5,7 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = []
-    for i in np.arange(53,58,2):
+    for i in np.arange(53,70,2):
         a = "0"+str(i)+"000"
         tr_filename.append(a)
     IN_LS = np.array([300,4]) #input shape in input layer - Stokes profiles as input
@@ -13,7 +13,7 @@ def main():
     sun_model = NN_model_atm("Stokes params", create_scaler=False)
     sun_model.compile_model(IN_LS, learning_rate=0.001)
     for fln in tr_filename:
-        sun_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=1)
+        sun_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=10)
         sun_model.plot_loss()
     sun_model.save_model()
 
