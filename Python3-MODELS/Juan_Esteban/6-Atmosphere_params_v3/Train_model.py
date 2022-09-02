a23f652b907c49b4d9e41565b3cd59b6f105dde8
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-01 21:10:13 -0500

    non existing files discarded

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v3/Train_model.py
@@ -5,9 +5,13 @@ def main():
     #Intensity specifications
     ptm = "/mnt/scratch/juagudeloo/Total_MURAM_data/"
     tr_filename = []
-    for i in np.arange(80,100,1):
-        a = "0"+str(i)+"000"
-        tr_filename.append(a)
+    non_existing = [85, 88, 89, 94, 95]
+    for i in np.arange(85,100,1):
+        if i in non_existing:
+            None
+        else:
+            a = "0"+str(i)+"000"
+            tr_filename.append(a)
     IN_LS = np.array([300,4]) #input shape in input layer - Stokes profiles as input
     #Model training
     sun_model = NN_model_atm("Stokes params", create_scaler=False)
