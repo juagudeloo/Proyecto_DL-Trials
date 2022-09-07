97fce761ae60105828e03897e7a4b29f35098a14
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-06 22:59:13 -0500

    de scaling

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -2,14 +2,19 @@ from this import d
 from venv import create
 import matplotlib.pyplot as plt
 import numpy as np
-from data_class import Data_class
+from data_class import Data_class, inverse_scaling
 
 def main():
     obtained_file = int(input("File of the obtained atmosphere values: "))
     filename = "0"+str(obtained_file)
     atm_params = np.load(f"obtained_value-0{obtained_file}.npy")
     data = Data_class(create_scaler=False)
-    original_atm_params = data.charge_atm_params(filename)
+    original_atm = data.charge_atm_params(filename)
+    scaler_names = ["mbyy", "mvyy", "mrho", "mtpr"]
+    for i in range(len(scaler_names)):
+        original_atm[:,:,i,:] = np.memmap.reshape(inverse_scaling(original_atm[:,:,i,:], scaler_names[i]), (480,480,(256-180)))
+
+
     height = 10
     max_height = len(atm_params[0,0,0,:])
     max_values = {}
@@ -21,11 +26,11 @@ def main():
     fig, ax = plt.subplots(3,4,figsize=(40,40))
     for i in range(4):
         ax[0,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "generated params")
-        ax[0,i].plot(np.arange(0,max_height,1)+1, original_atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
+        ax[0,i].plot(np.arange(0,max_height,1)+1, original_atm[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
         ax[0,i].set_title("Height serie in a maximum")
         ax[0,i].legend()
         ax[1,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "generated params")
-        ax[1,i].plot(np.arange(0,max_height,1)+1, original_atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
+        ax[1,i].plot(np.arange(0,max_height,1)+1, original_atm[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
         ax[1,i].set_title("Height serie in a minimum")
         ax[1,i].legend()
         ax[2,i].imshow(atm_params[:,:,i,height], cmap="gist_gray")
