ac219f7fc189eede8f5f29accc853c67eaac5b48
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-06 22:22:36 -0500

    testing

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -5,8 +5,13 @@ def main():
     obtained_file = int(input("File of the obtained atmosphere values: "))
     atm_params = np.load(f"obtained_value-0{obtained_file}000.npy")
     height = 10
-    max_min_mbyy = np.argwhere(atm_params[:,:,0,height]==np.max(atm_params[:,:,0,height]))
-    print(atm_params[max_min_mbyy[0][0], max_min_mbyy[0][1], 0, height])
+    max_values = {}
+    min_values = {}
+    for i in range(4):
+        max_values[i] = np.argwhere(atm_params[:,:,i,height]==np.max(atm_params[:,:,i,height]))
+        min_values[i] = np.argwhere(atm_params[:,:,i,height]==np.min(atm_params[:,:,i,height]))
+        
+    print(atm_params[max_values[0][0][0], max_values[0][0][1], 0, height])
 
 
 if __name__ == "__main__":
