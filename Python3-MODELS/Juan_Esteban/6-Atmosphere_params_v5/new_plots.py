219bab3e22a161c968b2b6b8ec3f92713605efbb
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-06 22:37:24 -0500

    plot height serie

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -5,6 +5,7 @@ def main():
     obtained_file = int(input("File of the obtained atmosphere values: "))
     atm_params = np.load(f"obtained_value-0{obtained_file}000.npy")
     height = 10
+    max_height = len(atm_params[0,0,0,:])
     max_values = {}
     min_values = {}
     for i in range(4):
@@ -16,9 +17,9 @@ def main():
 
     fig, ax = plt.subplots(3,4,figsize=(40,40))
     for i in range(4):
-        ax[0,i].plot(np.arange(0,height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i])
+        ax[0,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i])
         ax[0,i].title("Height serie in a maximum")
-        ax[1,i].plot(np.arange(0,height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i])
+        ax[1,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i])
         ax[1,i].title("Height serie in a minimum")
         ax[2,i].imshow(atm_params[:,:,i,height])
         ax[2,i].scatter(max_values[i][0][0], max_values[i][0][1], label = "maximum", color = "r")
