37de3e84df3e56f354c94a1a37ce5c782c35179c
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-06 22:08:44 -0500

    testing

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    obtained_file = input("File of the obtained atmosphere values:")
+    obtained_file = int(input("File of the obtained atmosphere values: "))
     atm_params = np.load(f"obtained_value-0{obtained_file}000.npy")
     print(np.shape(atm_params))
 
