27e442822d25c9f16403f8b5e2691741b55314aa
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-06 23:08:39 -0500

    stablishing a common location value

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -23,19 +23,24 @@ def main():
         max_values[i] = np.argwhere(atm_params[:,:,i,height]==np.max(atm_params[:,:,i,height]))
         min_values[i] = np.argwhere(atm_params[:,:,i,height]==np.min(atm_params[:,:,i,height]))
     
+    #Location values obtained from the temperature data
+    x_plot = max_values[3][0][0]
+    z_plot = min_values[3][0][0]
+
     fig, ax = plt.subplots(3,4,figsize=(40,40))
     for i in range(4):
-        ax[0,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "generated params")
-        ax[0,i].plot(np.arange(0,max_height,1)+1, original_atm[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
+        
+        ax[0,i].plot(np.arange(0,max_height,1)+1, atm_params[x_plot, z_plot, i], label = "generated params")
+        ax[0,i].plot(np.arange(0,max_height,1)+1, original_atm[x_plot, z_plot, i], label = "original params")
         ax[0,i].set_title("Height serie in a maximum")
         ax[0,i].legend()
-        ax[1,i].plot(np.arange(0,max_height,1)+1, atm_params[max_values[i][0][0], max_values[i][0][1], i], label = "generated params")
-        ax[1,i].plot(np.arange(0,max_height,1)+1, original_atm[max_values[i][0][0], max_values[i][0][1], i], label = "original params")
+        ax[1,i].plot(np.arange(0,max_height,1)+1, atm_params[x_plot, z_plot, i], label = "generated params")
+        ax[1,i].plot(np.arange(0,max_height,1)+1, original_atm[x_plot, z_plot, i], label = "original params")
         ax[1,i].set_title("Height serie in a minimum")
         ax[1,i].legend()
         ax[2,i].imshow(atm_params[:,:,i,height], cmap="gist_gray")
-        ax[2,i].scatter(max_values[i][0][0], max_values[i][0][1], label = "maximum", color = "r")
-        ax[2,i].scatter(min_values[i][0][0], min_values[i][0][1], label = "minimun", color = "g")
+        ax[2,i].scatter(x_plot, z_plot, label = "maximum", color = "r")
+        ax[2,i].scatter(x_plot, z_plot, label = "minimun", color = "g")
         ax[2,i].legend()
     fig.savefig(f"Images/Stokes_params/height_serie_plots_0{obtained_file}.png")
     
