960eb461c0ba75c25b963674aeab5cabcf68e198
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-11 07:47:47 -0500

    new location of the ob

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v5/new_plots.py
@@ -55,7 +55,7 @@ def main():
         ax[2,i].legend()
         divider = make_axes_locatable(ax[2,i])
         cax = divider.append_axes('right', size='5%', pad=0.05)
-        fig.colorbar(im_i, cax=cax, orientation='horizontal')
+        fig.colorbar(im_i, cax=cax, orientation='vertical')
     fig.savefig(f"Images/Stokes_params/height_serie_plots_0{obtained_file}.png")
     
 
