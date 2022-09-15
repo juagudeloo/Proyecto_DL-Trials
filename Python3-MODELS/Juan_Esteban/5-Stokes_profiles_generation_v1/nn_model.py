21df1395afa3df20dd52fbe4e764711be7d79236
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-14 20:04:09 -0500

    prameters normalization

--- Python3-MODELS/Juan_Esteban/5-Stokes_profiles_generation_v1/nn_model.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_profiles_generation_v1/nn_model.py
@@ -98,6 +98,8 @@ class NN_model_atm(Data_class_Stokes):
         original_stokes = self.charge_stokes_params(self.pred_filename)
         ylabel = ["$I$ [ph]", "$Q/I$", "$U/I$", "$V/I$"]
         original_stokes = np.memmap.reshape(inverse_scaling(original_stokes, "stokes"), (self.nx,self.nz,self.nlam, 4))
+        for i in range(1,4):
+            self.predicted_values[:,:,:,i] = self.predicted_values[:,:,:,i]/self.predicted_values[:,:,:,0]
         print(f"{self.pred_filename} prediction done!")
         for i in range(N_profs):
             ax[0,i].plot(np.arange(6302,6302+10*self.nlam, 10), self.predicted_values[ix,iz,:,i], label="Generated curve")
