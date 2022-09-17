75268ff3bbf5bf6bc9827187c137f60ddc0a4a3e
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-09-16 23:47:51 -0500

    test

--- Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v6/new_plots.py
+++ Python3-MODELS/Juan_Esteban/5-Stokes_Profiles_v6/new_plots.py
@@ -27,7 +27,7 @@ def main():
     min_z_plot = min_values[3][0][1]
 
     titles = ["Magnetic field LOS", "Velocity LOS", "Density", "Temperature"]
-    ylabels = [r"$I$", r"$Q$", r"$U$", r"$V$"]
+    ylabels = [r"$I_{NORMALIZED}$", r"$Q_{NORMALIZED}$", r"$U_{NORMALIZED}$", r"$V_{NORMALIZED}$"]
     fig, ax = plt.subplots(1,4,figsize=(9,17))
     for i in range(4):
         ax[i].plot(np.arange(6302,6302+10*300, 10), stokes[max_x_plot, max_z_plot, i], label = "generated stokes")
@@ -37,72 +37,70 @@ def main():
         ax[i].set_xlabel("height pixels")
         ax[i].set_ylabel(ylabels[i])
         ax[i].ticklabel_format(style = "sci")
-
     fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-01.png")
 
-#
-#    for i in range(4):
-#        ax[i].plot(np.arange(6302,6302+10*300, 10), stokes[max_x_plot, max_z_plot, i], label = "generated stokes")
-#        ax[i].set_title(titles[i]+"in maximum")
-#        ax[i].legend()
-#        ax[i].set_xlabel("height pixels")
-#        ax[i].set_ylabel(ylabels[i])
-#        ax[i].ticklabel_format(style = "sci")
-#    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-02.png")
-#
-#    for i in range(4):
-#        ax[i].plot(np.arange(6302,6302+10*300, 10), original_stokes[max_x_plot, max_z_plot, i], label = "original stokes")
-#        ax[i].set_title(titles[i]+"in maximum")
-#        ax[i].legend()
-#        ax[i].set_xlabel("height pixels")
-#        ax[i].set_ylabel(ylabels[i])
-#        ax[i].ticklabel_format(style = "sci")
-#    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-03.png")
-#
-#
-#    fig, ax = plt.subplots(1,4,figsize=(9,17))
-#
-#    for i in range(4):
-#        ax[i].plot(np.arange(0,max_height,1)+1, stokes[min_x_plot, min_z_plot, i], label = "generated stokes")
-#        ax[i].plot(np.arange(0,max_height,1)+1, original_stokes[min_x_plot, min_z_plot, i], label = "original stokes")
-#        ax[i].set_title(titles[i]+"in minimum")
-#        ax[i].legend()
-#        ax[i].set_xlabel("height pixels")
-#        ax[i].set_ylabel(ylabels[i])
-#        ax[i].ticklabel_format(style = "sci")
-#    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-11.png")
-#
-#    for i in range(4):
-#        ax[i].plot(np.arange(0,max_height,1)+1, stokes[min_x_plot, min_z_plot, i], label = "generated stokes")
-#        ax[i].set_title(titles[i]+"in minimum")
-#        ax[i].legend()
-#        ax[i].set_xlabel("height pixels")
-#        ax[i].set_ylabel(ylabels[i])
-#        ax[i].ticklabel_format(style = "sci")
-#    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-12.png")
-#
-#    for i in range(4):
-#        ax[i].plot(np.arange(0,max_height,1)+1, original_stokes[min_x_plot, min_z_plot, i], label = "original stokes")
-#        ax[i].set_title(titles[i]+"in minimum")
-#        ax[i].legend()
-#        ax[i].set_xlabel("height pixels")
-#        ax[i].set_ylabel(ylabels[i])
-#        ax[i].ticklabel_format(style = "sci")
-#    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-13.png")
+    for i in range(4):
+        ax[i].plot(np.arange(6302,6302+10*300, 10), stokes[max_x_plot, max_z_plot, i], label = "generated stokes")
+        ax[i].set_title(titles[i]+"in maximum")
+        ax[i].legend()
+        ax[i].set_xlabel("height pixels")
+        ax[i].set_ylabel(ylabels[i])
+        ax[i].ticklabel_format(style = "sci")
+    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-02.png")
+
+    for i in range(4):
+        ax[i].plot(np.arange(6302,6302+10*300, 10), original_stokes[max_x_plot, max_z_plot, i], label = "original stokes")
+        ax[i].set_title(titles[i]+"in maximum")
+        ax[i].legend()
+        ax[i].set_xlabel("height pixels")
+        ax[i].set_ylabel(ylabels[i])
+        ax[i].ticklabel_format(style = "sci")
+    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-03.png")
 
 
-#    fig, ax = plt.subplots(1,4,figsize=(9,17))
-#
-#    for i in range(4):
-#        im_i = ax[i].imshow(stokes[:,:,i,wavelength], cmap="gist_gray")
-#        ax[i].scatter(max_x_plot, max_z_plot, label = "maximum", color = "r")
-#        ax[i].scatter(min_x_plot, min_z_plot, label = "minimun", color = "g")
-#        ax[i].set_title(titles[i])
-#        ax[i].legend()
-#        divider = make_axes_locatable(ax[i])
-#        cax = divider.append_axes('bottom', size='5%', pad=0.3)
-#        fig.colorbar(im_i, cax=cax, orientation="horizontal")
-#    fig.savefig(f"Images/Stokes_params/height_serie_plots_0{obtained_file}-2.png")
+    fig, ax = plt.subplots(1,4,figsize=(9,17))
+
+    for i in range(4):
+        ax[i].plot(np.arange(0,max_height,1)+1, stokes[min_x_plot, min_z_plot, i], label = "generated stokes")
+        ax[i].plot(np.arange(0,max_height,1)+1, original_stokes[min_x_plot, min_z_plot, i], label = "original stokes")
+        ax[i].set_title(titles[i]+"in minimum")
+        ax[i].legend()
+        ax[i].set_xlabel("height pixels")
+        ax[i].set_ylabel(ylabels[i])
+        ax[i].ticklabel_format(style = "sci")
+    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-11.png")
+
+    for i in range(4):
+        ax[i].plot(np.arange(0,max_height,1)+1, stokes[min_x_plot, min_z_plot, i], label = "generated stokes")
+        ax[i].set_title(titles[i]+"in minimum")
+        ax[i].legend()
+        ax[i].set_xlabel("height pixels")
+        ax[i].set_ylabel(ylabels[i])
+        ax[i].ticklabel_format(style = "sci")
+    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-12.png")
+
+    for i in range(4):
+        ax[i].plot(np.arange(0,max_height,1)+1, original_stokes[min_x_plot, min_z_plot, i], label = "original stokes")
+        ax[i].set_title(titles[i]+"in minimum")
+        ax[i].legend()
+        ax[i].set_xlabel("height pixels")
+        ax[i].set_ylabel(ylabels[i])
+        ax[i].ticklabel_format(style = "sci")
+    fig.savefig(f"Images/Stokes_params/stokes_plot_0{obtained_file}-13.png")
+
+
+    fig, ax = plt.subplots(1,4,figsize=(9,17))
+
+    for i in range(4):
+        im_i = ax[i].imshow(stokes[:,:,i,wavelength], cmap="gist_gray")
+        ax[i].scatter(max_x_plot, max_z_plot, label = "maximum", color = "r")
+        ax[i].scatter(min_x_plot, min_z_plot, label = "minimun", color = "g")
+        ax[i].set_title(titles[i])
+        ax[i].legend()
+        divider = make_axes_locatable(ax[i])
+        cax = divider.append_axes('bottom', size='5%', pad=0.3)
+        fig.colorbar(im_i, cax=cax, orientation="horizontal")
+    fig.savefig(f"Images/Stokes_params/height_serie_plots_0{obtained_file}-2.png")
     
 #
 
