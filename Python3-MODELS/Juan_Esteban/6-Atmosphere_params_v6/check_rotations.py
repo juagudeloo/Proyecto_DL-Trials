1efa27125c1da65baee9e3cd43a13656aa6654a9
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-10-02 13:11:51 -0500

    plot the images to check the rotations

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
@@ -11,5 +11,16 @@ def main():
     print(np.shape(intensity))
     print(np.shape(stokes))
 
+    fig, ax = plt.subplots(3,4, figsize = (32,7))
+    ax[0,0].imshow(atm_params[:,:,10,0])
+    ax[0,1].imshow(atm_params[:,:,10,1])
+    ax[0,2].imshow(atm_params[:,:,10,2])
+    ax[1,0].imshow(intensity)
+    ax[2,0].imshow(stokes[:,:,10,0])
+    ax[2,0].imshow(stokes[:,:,10,1])
+    ax[2,0].imshow(stokes[:,:,10,2])
+    ax[2,0].imshow(stokes[:,:,10,3])
+|
+
 if __name__ == "__main__":
     main()
