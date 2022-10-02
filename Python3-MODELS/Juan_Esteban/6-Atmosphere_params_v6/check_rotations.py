c164771b32f98bb6536421f01ef6ed3f81113a51
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-10-02 13:33:33 -0500

    set titles

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
@@ -6,22 +6,25 @@ def main():
     filename = "099000"
     sun = Data_class()
     atm_params = sun.charge_atm_params(filename)
+    atm_titles = ["mbyy", "mvyy", "mrho", "mtpr"]
     intensity = sun.charge_intensity(filename)
     stokes = sun.charge_stokes_params(filename)
+    stokes_titles = ["I", "Q", "U", "V"]
     print(np.shape(atm_params))
     print(np.shape(intensity))
     print(np.shape(stokes))
 
     fig, ax = plt.subplots(3,4, figsize = (32,7))
-    ax[0,0].imshow(atm_params[:,:,10,0])
-    ax[0,1].imshow(atm_params[:,:,10,1])
-    ax[0,2].imshow(atm_params[:,:,10,2])
-    ax[0,3].imshow(atm_params[:,:,10,3])
+    for i in range(4)
+        ax[0,i].imshow(atm_params[:,:,10,i])
+        ax[0,i].set_title(atm_titles[i])
+        
+        ax[2,i].imshow(stokes[:,:,10,i])
+        ax[2,i].set_title(stokes_titles[i])
+
     ax[1,0].imshow(intensity)
-    ax[2,0].imshow(stokes[:,:,10,0])
-    ax[2,1].imshow(stokes[:,:,10,1])
-    ax[2,2].imshow(stokes[:,:,10,2])
-    ax[2,3].imshow(stokes[:,:,10,3])
+    ax[1,0].set_title("Intensity")
+
     fig.savefig("Images/rotations.png")
 
 
