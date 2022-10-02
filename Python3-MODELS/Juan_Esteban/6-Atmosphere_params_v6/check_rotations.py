3117c26b3028270bcc8c1409ed56148220b47eb9
Juan Esteban Agudelo Ortiz <juanessao2000@pop-os.localdomain>
2022-10-02 12:44:47 -0500

    fix

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v6/check_rotations.py
@@ -6,11 +6,11 @@ def main():
     sun = Data_class
     atm_params = sun.charge_atm_params(filename)
     intensity = sun.charge_intensity(filename)
-    stokes = sun.charge_stokes_params
+    stokes = sun.charge_stokes_params(filename)
     print(np.shape(atm_params))
     print(np.shape(intensity))
     print(np.shape(stokes))
-
+c
 
 if __name__ == "__main__":
     main()
