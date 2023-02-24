9856e86d8435f6208280803be140732fa51d465a
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-02-24 11:55:54 -0500

    check the running of the cod

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Atm-Stokes_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Atm-Stokes_model.py
@@ -22,6 +22,7 @@ def main():
     
     start_time = time.time() #Time measured in seconds
     for fln in tr_filename:
+        print("is running")
         atm_model.train(fln, tr_s = 0.75, batch_size= 100, epochs=100)
         atm_model.plot_loss()
 
