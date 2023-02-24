8f59a8f23f32808e2465056e3b5720e2529a20ea
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-02-24 11:46:48 -0500

    less batch_size

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Stokes-Atm_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Stokes-Atm_model.py
@@ -24,7 +24,7 @@ def main():
     
     start_time = time.time() #Time measured in seconds
     for fln in tr_filename:
-        stokes_model.train(fln, tr_s = 0.75, batch_size= 10000, epochs=100)
+        stokes_model.train(fln, tr_s = 0.75, batch_size= 100, epochs=100)
         stokes_model.plot_loss()
 
     with open(f"{stokes_model.nn_model_type}/training/training_time.txt", "w") as f:
