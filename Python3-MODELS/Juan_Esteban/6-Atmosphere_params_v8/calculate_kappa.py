ff1c47a37b1e93d05b40170855ea958adba87865
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-03-30 08:46:50 -0500

    fixing the top value

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/calculate_kappa.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/calculate_kappa.py
@@ -2,14 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.interpolate import LinearNDInterpolator
-from path import path
+from path import path_LOCAL, path_UIS
 from train_generate.data_class import DataClass
 from scipy.integrate import simps
 
 
 def main():
-    ptm = path()
+    #path to kappa.0 file
+    ptm = path_UIS()
     kappa_df = pd.read_table(ptm+"kappa.0.dat", sep=" ", header=None, skipinitialspace=True).dropna(axis=1, how = "all")
+
+    #Temperature and Pressure values of the training grid
     tab_T=np.array([3.32, 3.34, 3.36, 3.38, 3.40, 3.42, 3.44, 3.46, 3.48, 3.50,
     3.52, 3.54, 3.56, 3.58, 3.60, 3.62, 3.64, 3.66, 3.68, 3.70,
     3.73, 3.76, 3.79, 3.82, 3.85, 3.88, 3.91, 3.94, 3.97, 4.00,
@@ -19,44 +22,44 @@ def main():
 
     tab_p=np.array([-2., -1.5, -1., -.5, 0., .5, 1., 1.5, 2., 2.5,
         3., 3.5, 4., 4.5, 5., 5.5, 6. ,6.5, 7., 7.5, 8.])
-    
-    tab_T=np.array([3.32, 3.34, 3.36, 3.38, 3.40, 3.42, 3.44, 3.46, 3.48, 3.50,
-    3.52, 3.54, 3.56, 3.58, 3.60, 3.62, 3.64, 3.66, 3.68, 3.70,
-    3.73, 3.76, 3.79, 3.82, 3.85, 3.88, 3.91, 3.94, 3.97, 4.00,
-    4.05, 4.10, 4.15, 4.20, 4.25, 4.30, 4.35, 4.40, 4.45, 4.50,
-    4.55, 4.60, 4.65, 4.70, 4.75, 4.80, 4.85, 4.90, 4.95, 5.00,
-    5.05, 5.10, 5.15, 5.20, 5.25, 5.30 ])
 
-    tab_p=np.array([-2., -1.5, -1., -.5, 0., .5, 1., 1.5, 2., 2.5,
-        3., 3.5, 4., 4.5, 5., 5.5, 6. ,6.5, 7., 7.5, 8.])
-
+    #Selecting the positions in (T, P) corresponding to each kappa value.
+    #Remember that this values are the base 10 logarithm of the original ones.
     T = np.take(tab_T, kappa_df[0])
     P = np.take(tab_p, kappa_df[1])
 
+    #Organizing the training points and its values
     TP_points =list(zip(T, P))
     kappa = kappa_df[2].to_numpy()
-    f = LinearNDInterpolator(TP_points, kappa)
 
+    #Training the linear interpolator
+    f = LinearNDInterpolator(TP_points, kappa)
+    
+    #Charging the values of Temperature and Pressure from a snapshot of the MURAM simulation
     filename = "175000"
     kappa_C = KappaClass(ptm)
     kappa_C.charge_TP(filename)
     
-
+    #finding the base 10 logarithm of the snapshot values
     T_muram = np.log10(kappa_C.mtpr[:,:,:])
     P_muram = np.log10(kappa_C.mprs[:,:,:])
+
+    #Obtaining the corresponding inteporlated values of the MURAM snapshot
     kappa_cube = f(T_muram, P_muram)
 
-    # Array for y distance in meters values
+    # Array for y distance in meters values (from 0 to 2560 in dy = 10 km steps)
     Y = np.arange(0,256*10,10.)
 
+    #Creation of the array to store the optical depth values
     opt_depth = np.zeros((kappa_C.nx,kappa_C.ny,kappa_C.nz))
 
     #Calculating the optical depth with the kappa integral
     for ix in range(kappa_C.nx):
         for iz in range(kappa_C.nz):
             for iy in range(kappa_C.ny):
+                #The first value of the top 
                 if iy == 0:
-                    opt_depth[ix,kappa_C.ny-1,iz] = kappa_cube[ix,kappa_C.ny-1,iz]
+                    opt_depth[ix,kappa_C.ny-1,iz] = 1e-2
                 else:
                     print(len(kappa_cube[ix,kappa_C.ny-1-iy:,iz]))
                     print(len(Y[kappa_C.ny-1-iy:]))
