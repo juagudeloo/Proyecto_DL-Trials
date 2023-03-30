b1684417380f19b04d0b7bae978a003497fa348a
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-03-30 08:28:22 -0500

    path in local and uis

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/path.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/path.py
@@ -1,2 +1,5 @@
-def path():
-    return "/girg/juagudeloo/MURAM_data/"
+def path_UIS():
+    return "/media/hdd/PRINCIPAL-2022-2/PROYECTOS/PROYECTO_DL/MURAM_data/"
+
+def path_LOCAL():
+    return "/media/hdd/PRINCIPAL-2022-2/PROYECTOS/PROYECTO_DL/MURAM_data/"
