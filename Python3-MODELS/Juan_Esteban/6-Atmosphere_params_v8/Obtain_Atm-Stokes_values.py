37d60d9d46536236c439497b0beca43e775b861b
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-06-03 14:25:53 -0500

    checking

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/Obtain_Atm-Stokes_values.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/Obtain_Atm-Stokes_values.py
@@ -10,18 +10,21 @@ def main():
     light_model.compile_model()
     light_model.load_weights(f"{light_model.nn_model_type}/training/{light_model.light_type}/cp.ckpt")
     #Model predicting
-    pr_filename = []
-    for i in np.arange(175,200, 2):
-        if i <100:
-            a = "0"+str(i)+"000"
-            pr_filename.append(a)
-        else:
-            a = str(i)+"000"
-            pr_filename.append(a)
-    
-    for fln in pr_filename:
-        light_model.predict_values(fln)
-        light_model.plot_predict(fln)
+    fln = "175000"
+    light_model.predict_values(fln)
+    light_model.plot_predict(fln)
+    #pr_filename = []
+    #for i in np.arange(175,200, 2):
+    #    if i <100:
+    #        a = "0"+str(i)+"000"
+    #        pr_filename.append(a)
+    #    else:
+    #        a = str(i)+"000"
+    #        pr_filename.append(a)
+    #
+    #for fln in pr_filename:
+    #    light_model.predict_values(fln)
+    #    light_model.plot_predict(fln)
 
 
 if __name__ == "__main__":
