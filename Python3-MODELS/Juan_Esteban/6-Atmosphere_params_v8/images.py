4dc52306b8af1ad5458213c95738a6d1c9b6d6d9
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-06-13 00:32:53 -0500

    images for tdg

---
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8/images.py
@@ -0,0 +1,36 @@
+from path import path_LOCAL
+from train_generate.data_class import DataClass
+import matplotlib.pyplot as plt 
+import numpy as np
+
+def main():
+    ptm = path_UIS()
+    muram = DataClass(ptm)
+
+    filename = "087000"
+    stokes_87 = muram.charge_stokes_params(filename, scale = False)
+    threshold_87 = (np.max(stokes_87)-np.min(stokes_87))/2.5 + np.min(stokes_87)
+    gran_mask_87 = np.ma.masked_where(stokes_87 > threshold_87, stokes_87).mask
+    intergran_mask_87 = np.ma.masked_where(stokes_87 <= threshold_87, stokes_87).mask
+
+    filename = "175000"
+    stokes_175 = muram.charge_stokes_params(filename, scale = False)
+    threshold_175 = (np.max(stokes_175)-np.min(stokes_175))/2.5 + np.min(stokes_175)
+    gran_mask_175 = np.ma.masked_where(stokes_175 > threshold_175, stokes_175).mask
+    intergran_mask_175 = np.ma.masked_where(stokes_175 <= threshold_175, stokes_175).mask
+
+    fig, ax = plt.subplots(1,2,figsize = (12,5))
+    ax[0].imshow(stokes_87[:,:,0,0])
+    ax[0].imshow(gran_mask_87, alpha = 0.6)
+    ax[0].imshow(intergran_mask_87, alpha = 0.6)
+    
+    ax[1].imshow(stokes_175[:,:,0,0])
+    ax[1].imshow(gran_mask_175, alpha = 0.6)
+    ax[1].imshow(intergran_mask_175, alpha = 0.6)
+
+    fig.savefig("int_gran_zones.pdf")
+
+
+
+if __name__ == "__main__":
+    main()
