e25b0f38ee21e6268adeded32c5f9b5e113b45d3
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-06-21 23:44:17 -0500

    update Atm Stokes

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Atm-Stokes_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/Train_Atm-Stokes_model.py
@@ -2,29 +2,25 @@ import numpy as np
 from train_generate.nn_model import LightObtainModel
 import time
 from path import path
+import sys
 
 def main():
     #Intensity specifications
     ptm = path()
-    tr_filename = []
-    for i in np.arange(80,150+1,14):
-        if i==98:
-            None
-        else:
-            if i<100:
-                a = "0"+str(i)+"000"
-                tr_filename.append(a)
-            else:
-                a = str(i)+"000"
-                tr_filename.append(a)
+
+    ifl = int(sys.argv[1])
+    if ifl < 100:
+        fln = "0"+str(ifl)+"000"
+    else:
+        fln = str(ifl)+"000"
+
     atm_model = LightObtainModel(ptm = ptm, light_type="Stokes params", create_scaler=False)
     atm_model.compile_model(learning_rate=0.001)
     
     start_time = time.time() #Time measured in seconds
-    for fln in tr_filename:
-        print("is running")
-        atm_model.train(fln, tr_s = 0.75, batch_size= 100, epochs=100)
-        atm_model.plot_loss()
+    print("is running")
+    atm_model.train(fln, tr_s = 0.75, batch_size= 10000, epochs=40)
+    atm_model.plot_loss()
 
     with open(f"{atm_model.nn_model_type}/training/training_time.txt", "w") as f:
         f.write(f"{(time.time()-start_time)*(1/3600)} hours")
