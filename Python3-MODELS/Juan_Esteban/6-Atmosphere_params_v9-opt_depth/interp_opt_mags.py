17778e6ab2964dae9804cdfec8abee9b3a81edf9
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-06-29 02:12:14 -0500

    fix

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/interp_opt_mags.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/interp_opt_mags.py
@@ -13,22 +13,22 @@ def main():
 
     filename = "175000"
     opt_depth = np.load("optical_depth_"+filename+".npy")
-
-    muram.charge_atm_params(filename)
-
-    mags = ["By_opt", "Vy_opt", "log_rho_opt", "T_opt"]
-    opt_mags_interp = {}
-    for i in range(4):
-        opt_mags_interp[mags[i]] = interp1d(opt_depth[ix,:,iz], muram.atm_params[ix,:,iz,i])
-    
-    opt_grid = np.arange(-2,5,1)
-
-    fig, ax = plt.subplots(1,4,figsize=(7,7))
-    for i in range(4):
-        ax[i].plot(opt_grid, opt_mags_interp[mags[i]](opt_grid))
-        ax[i].set_title(mags[i])
-
-    fig.savefig("optical_depth_mapping"+filename+".pdf")
+    print(opt_depth[ix,:,iz].shape)
+    #muram.charge_atm_params(filename)
+#
+    #mags = ["By_opt", "Vy_opt", "log_rho_opt", "T_opt"]
+    #opt_mags_interp = {}
+    #for i in range(4):
+    #    opt_mags_interp[mags[i]] = interp1d(opt_depth[ix,:,iz], muram.atm_params[ix,:,iz,i])
+    #
+    #opt_grid = np.arange(-2,5,1)
+#
+    #fig, ax = plt.subplots(1,4,figsize=(7,7))
+    #for i in range(4):
+    #    ax[i].plot(opt_grid, opt_mags_interp[mags[i]](opt_grid))
+    #    ax[i].set_title(mags[i])
+#
+    #fig.savefig("optical_depth_mapping"+filename+".pdf")
 
 
 
