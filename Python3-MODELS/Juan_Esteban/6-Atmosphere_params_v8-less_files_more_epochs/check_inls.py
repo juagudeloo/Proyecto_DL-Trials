f601cb50470279256770209e20498ef153d8b0bb
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-07-11 13:55:36 -0500

    add top boundary

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/check_inls.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/check_inls.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def main():
     ptm = path()
-    muram = LightObtainModel(ptm = ptm)
+    muram = LightObtainModel(ptm = ptm, low_boundary = low_boundary(), top_boundary = top_boundary())
     fln = "175000"
-    muram.split_data_light_output(filename = fln,  light_type = "Stokes params", low_boundary = low_boundary(), top_boundary = top_boundary(), TR_S = 0.7)
+    muram.split_data_light_output(filename = fln,  light_type = "Stokes params", TR_S = 0.7)
     print(muram.in_ls)
 
 if __name__ == "__main__":
