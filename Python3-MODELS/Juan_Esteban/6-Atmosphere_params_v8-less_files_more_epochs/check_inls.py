43adcc0ff6ffaa7fc50096916af10cf26ed9c3a2
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-07-11 07:46:09 -0500

    fix

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/check_inls.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v8-less_files_more_epochs/check_inls.py
@@ -3,7 +3,7 @@ from train_generate.data_class import DataClass
 import numpy as np
 
 def main():
-    ptm = path
+    ptm = path()
     muram = DataClass(ptm = ptm)
     fln = "175000"
     muram.split_data_light_output(filename = fln,  light_type = "Stokes params", TR_S = 0.7)
