c5ee8062ca141ee7703b32d7a0c254a0c5993406
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-09-06 23:10:13 -0500

    unuseful argument

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/train_generate/nn_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/train_generate/nn_model.py
@@ -7,15 +7,13 @@ from train_generate.boundaries import low_boundary, top_boundary
 ################################################################################################################
 
 class AtmObtainModel(DataClass, AtmTrainVisualMixin, NN_ModelCompileMixin):
-    def __init__(self, ptm, opt_len, nx = 480, ny = 256, nz = 480, low_boundary = low_boundary(), top_boundary = top_boundary(), create_scaler = False, 
+    def __init__(self, ptm, nx = 480, ny = 256, nz = 480, low_boundary = low_boundary(), top_boundary = top_boundary(), create_scaler = False, 
                 light_type = "Intensity"):
-        self.opt_len = opt_len
         DataClass.__init__(self, ptm, nx, ny, nz, low_boundary, top_boundary, create_scaler, light_type)
         AtmTrainVisualMixin.__init__(self)
         
 class LightObtainModel(DataClass, LightTrainVisualMixin, NN_ModelCompileMixin):
-    def __init__(self, ptm, opt_len, nx = 480, ny = 256, nz = 480,  low_boundary = low_boundary(), top_boundary = top_boundary(), create_scaler = False, 
+    def __init__(self, ptm, nx = 480, ny = 256, nz = 480,  low_boundary = low_boundary(), top_boundary = top_boundary(), create_scaler = False, 
                 light_type = "Intensity"):
-        self.opt_len = opt_len
         DataClass.__init__(self, ptm, nx, ny, nz, low_boundary, top_boundary, create_scaler, light_type)
         LightTrainVisualMixin.__init__(self)
