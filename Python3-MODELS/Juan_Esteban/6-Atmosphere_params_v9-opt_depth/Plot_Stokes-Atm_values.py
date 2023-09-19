996884dbbd18298b2bbcb886a8bb2c54dafc9f5f
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-09-19 13:10:11 -0500

    testing plots

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/Plot_Stokes-Atm_values.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/Plot_Stokes-Atm_values.py
@@ -6,42 +6,22 @@ from train_generate.data_class import inverse_scaling
 def main():
     ptm = path_UIS()
     atm_model = AtmObtainModel(ptm = ptm, light_type="Stokes params", create_scaler=False)
-    filenames = []
-    xz_coords = {}
-    xz_titles = {}
-    for num in np.arange(175,201,2):
-        fln = str(num)+"000"
-        filenames.append(fln)
-        xz_coords[fln] = []
 
-    iz = 280
-    xz_coords["175000"] = np.array([[84,iz],
-                                     [220,iz],
-                                     [250,iz],
-                                     [350,iz]])
-    xz_titles["175000"] = ["intergranular",
-                           "granular",
-                           "intergranular",
-                           "granular"]
+    #Model training
+    #atm_model.compile_model()
+    #atm_model.load_weights(f"{atm_model.nn_model_type}/training/{atm_model.light_type}/cp.ckpt")
+    #Model predicting
+    pr_filename = []
+    #for i in np.arange(175, 200, 2):
+    #    if i <100:
+    #        a = "0"+str(i)+"000"
+    #        pr_filename.append(a)
+    #    else:
+    #        a = str(i)+"000"
+    #        pr_filename.append(a)
 
-    xz_coords["177000"] = np.array([[30,iz],
-                                     [120,iz],
-                                     [252,iz],
-                                     [300,iz]])
-    xz_titles["177000"] = ["granular",
-                           "granular",
-                           "intergranular",
-                           "granular"]
-
-
-    for fln in filenames:
-        ## Plot for checking the stokes parameters spatial distribution for a given value of ix or iz
-
-    	#atm_model.plot_predict_initial(fln)
-
-        ## Plot the atmosphere params for given ix and iz values
-
-        atm_model.plot_predict_specific(fln, xz_coords[fln], xz_titles[fln])
+    for fln in pr_filename:
+        atm_model.plot_predict(fln)
 
 if __name__ == "__main__":
     main()
