66766069a127a212ce5898fad26b98ccbf82f040
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-10-12 14:36:13 -0500

    testing

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/select_filename_train_Stokes-Atm.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/select_filename_train_Stokes-Atm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import subprocess
-path = "/girg/juagudeloo/Proyecto_DL-Trials/Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth"
+path = "/girg/juagudeloo/Proyecto_DL-Trials/Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/"
 
 subprocess.call(path+'Begin_Train_Stokes-Atm_model.py 080000', shell = True)
 subprocess.call(path+'Train_Stokes-Atm_model.py 083000', shell = True)
