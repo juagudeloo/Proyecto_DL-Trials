d87a08471e2b552cd231858b4f02d7d74d87bf38
juanessao2000 <juanessao2000@pop-os.localdomain>
2023-10-18 22:50:49 -0500

    testing done

--- Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/Train_Stokes-Atm_model.py
+++ Python3-MODELS/Juan_Esteban/6-Atmosphere_params_v9-opt_depth/Train_Stokes-Atm_model.py
@@ -7,42 +7,33 @@ import sys
 def main():
     #Intensity specifications
     ptm = path_UIS()
-    fln = str(sys.argv[1])
+    tr_filename = []
     print(f"######################## filename{fln}")
-    
-    
-    stokes_model = AtmObtainModel(ptm = ptm, light_type="Stokes params", create_scaler=False)
-    stokes_model.compile_model(learning_rate=0.001)
-    stokes_model.load_weights(f"{stokes_model.nn_model_type}/training/{stokes_model.light_type}/cp.ckpt")
-
-    start_time = time.time() #Time measured in seconds
-    stokes_model.train(fln, tr_s = 0.75, batch_size= 1000, epochs=1)
-    stokes_model.plot_loss()
 
     with open(f"{stokes_model.nn_model_type}/training/training_time_{fln}.txt", "w") as f:
         f.write(f"{(time.time()-start_time)*(1/3600)} hours")
 
-    #for i in np.arange(80,94+1,7):
-    #    if i==98:
-    #        None
-    #    else:
-    #        if i<100:
-    #            a = "0"+str(i)+"000"
-    #            tr_filename.append(a)
-    #        else:
-    #            a = str(i)+"000"
-    #            tr_filename.append(a)
-    #
-    #stokes_model = AtmObtainModel(ptm = ptm, light_type="Stokes params", create_scaler=False)
-    #stokes_model.compile_model(learning_rate=0.001)
-    #
-    #start_time = time.time() #Time measured in seconds
-    #for fln in tr_filename:
-    #    stokes_model.train(fln, tr_s = 0.75, batch_size= 10000, epochs=40)
-    #    stokes_model.plot_loss()
-#
-    #with open(f"{stokes_model.nn_model_type}/training/training_time.txt", "w") as f:
-    #    f.write(f"{(time.time()-start_time)*(1/3600)} hours")
+    for i in np.arange(80,94+1,7):
+        if i==98:
+            None
+        else:
+            if i<100:
+                a = "0"+str(i)+"000"
+                tr_filename.append(a)
+            else:
+                a = str(i)+"000"
+                tr_filename.append(a)
+    
+    stokes_model = AtmObtainModel(ptm = ptm, light_type="Stokes params", create_scaler=False)
+    stokes_model.compile_model(learning_rate=0.001)
+    
+    start_time = time.time() #Time measured in seconds
+    for fln in tr_filename:
+        stokes_model.train(fln, tr_s = 0.75, batch_size= 10000, epochs=40)
+        stokes_model.plot_loss()
+
+    with open(f"{stokes_model.nn_model_type}/training/training_time.txt", "w") as f:
+        f.write(f"{(time.time()-start_time)*(1/3600)} hours")
 
 if __name__ == "__main__":
     main()
